86927051aca4345a3be7a9115371600ba556a5e3
vslutov <vslutov@yandex.ru>
2015-04-18 22:20:44 +0300

    Split models to package

--- grmaster/models/group.py
+++ grmaster/models/group.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-Models of groups, streams, partitions etc.
+Basic group type.
+
+It's not only academic group.
 """
 
 #   group-master - tool for divide students into groups
@@ -20,7 +22,6 @@ Models of groups, streams, partitions etc.
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .utils import Table
 
 class Group(list):
     def __init__(self, arg=[]):
@@ -30,30 +31,3 @@ class Group(list):
         else:
             super(Group, self).__init__(arg)
             self.max_size = len(self)
-
-class Stream(list):
-    def __init__(self, size):
-        super(Stream, self).__init__(Group for i in range(size))
-        self.size = size
-
-
-class Streams(list):
-    def __init__(self, stream_sizes):
-        super(Streams, self).__init__(Stream(size) for size in stream_sizes)
-
-    def get_size(self):
-        return sum(len(group) for group in self)
-
-
-class Partition(object):
-    def __init__(self, student_table, stream_sizes):
-        if type(student_table) is Table:
-            self.students = student_table
-        else:
-            self.students = Table.from_file(student_table)
-        self.total_groups = sum(stream_sizes)
-        self.streams = Streams(stream_sizes)
-
-    # TODO: move to another file
-    def english_rule(self, english_header):
-        aggregates = self.students.split_by_header(english_header)
---
+++ grmaster/models/partition.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+Intermediate form of information about groups (without rules).
+"""
+
+#   group-master - tool for divide students into groups
+#   Copyright (C) 2015  Lutov V. S. <vslutov@yandex.ru>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class Stream(list):
+    def __init__(self, size):
+        super(Stream, self).__init__(Group for i in range(size))
+        self.size = size
+
+
+class Streams(list):
+    def __init__(self, stream_sizes):
+        super(Streams, self).__init__(Stream(size) for size in stream_sizes)
+
+    def get_size(self):
+        return sum(len(group) for group in self)
+
+
+class Partition(object):
+    def __init__(self, student_table, stream_sizes):
+        if type(student_table) is Table:
+            self.students = student_table
+        else:
+            self.students = Table.from_file(student_table)
+        self.total_groups = sum(stream_sizes)
+        self.streams = Streams(stream_sizes)
+
+    # TODO: move to another file
+    def english_rule(self, english_header):
+        aggregates = self.students.split_by_header(english_header)
