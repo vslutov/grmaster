2e58ee278703a137a602a9f92a4600ed7fdda115
vslutov <vslutov@yandex.ru>
2015-04-19 01:34:01 +0300

    Data for testing

---
+++ grmaster/data/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Standard test tables.
+"""
+
+#   group-master - tool for divide students into groups
+#   Copyright (C) 2015  Lutov V. S. <vslutov@yandex.ru>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from grmaster.utils import Table
+
+data_dir = os.path.abspath(os.path.dirname(__file__))
+
+def load(filename):
+    return Table.from_csv_file(os.path.join(data_dir, filename))
+
+def load_students():
+    return load('students.csv')
---
+++ grmaster/data/tests/test_data.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for standard test tables.
+"""
+
+#   group-master - tool for divide students into groups
+#   Copyright (C) 2015  Lutov V. S. <vslutov@yandex.ru>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from grmaster import data
+from grmaster.utils import Table
+
+
+def test_load():
+    table = data.load('students.csv')
+    assert(type(table) is Table)
+    assert(len(table) > 0)
+
+
+def test_load_students():
+    assert(data.load('students.csv') == data.load_students())
